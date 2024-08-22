d81ff36dc4c6d6c8a9b215d1a5e75ef3d15489bb
Samuel Chandler <sec180005@utdallas.edu>
2024-08-22 15:38:35 -0500

    Formatting Data Frame

--- ToImage.py
+++ ToImage.py
@@ -3,9 +3,19 @@ from PIL import Image
 
 RESULT_NAME = "Result.xlsx"
 
-dataframe1 = pd.read_excel(RESULT_NAME)
+dataFrame = pd.read_excel(RESULT_NAME)
 
-print(dataframe1)
+#removing Unneeded Variables 
+dataFrame.pop("Var4")
+dataFrame.pop("Var5")
+dataFrame.pop("Var6")
+dataFrame.pop("Var7")
+dataFrame.pop("Var8")
+dataFrame.pop("Var9")
+
+readings = pd.DataFrame(dataFrame[1:])
+
+print(readings)
 
 im = Image.new(mode="RGB", size=(200,200), color='red')
 
