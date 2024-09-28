ab580680e9eceee27c83e852faca9b7d224e8ee0
ajcan203 <aj.cantu0606@gmail.com>
2024-09-28 16:47:56 -0500

    added request for center frequency. Also figure check to end the while loop

--- PythonScripts/hackRFtrial_spectrumanalyzer.py
+++ PythonScripts/hackRFtrial_spectrumanalyzer.py
@@ -2,12 +2,13 @@ from hackrf import *
 from scipy import signal
 from time import sleep
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     with HackRF() as hrf:
         
         hrf.sample_rate = 20e6
-        hrf.center_freq = 2400e6
+        hrf.center_freq = np.int16(input('Please enter center frequncy in MHz:'))*1e6
         hrf.lna_gain = 8
         hrf.vga_gain = 22
         num_samples = 2e4
@@ -16,26 +17,27 @@ def main():
         
         f, pxx = signal.welch(samples, hrf.sample_rate/1e6, 'blackman',1024)
     
-        
+        y_center = hrf.center_freq/1e6
 
         plt.ion()
 
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        line1, = ax.plot(np.fft.fftshift(f)+1420, np.fft.fftshift(10*np.log10(pxx)))
+        line1, = ax.plot(np.fft.fftshift(f)+y_center, np.fft.fftshift(10*np.log10(pxx)))
         plt.ylabel('Power Density (dB/Hz)')
         plt.xlabel('Frequency (MHz)')
         plt.title('Spectrum Analyzer')
         plt.ylim(-70,10)
 
 
-        while 1: 
+        while plt.fignum_exists(fig.number): 
             sleep(.2)
             samples = hrf.read_samples(2e4)[500:]
             f,pxx = signal.welch(samples, hrf.sample_rate/1e6, 'blackman', 1024)
             line1.set_ydata(np.fft.fftshift(10*np.log10(pxx)))
             fig.canvas.draw()
             fig.canvas.flush_events()
+    return
 
        
 
