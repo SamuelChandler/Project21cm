fixes to magreader script. Script will now return an averaged value across a band rather than one point. number of averages can be altered within script.

PythonScripts/hackRF_magreader.py
@@ -1,35 +1,55 @@
 from hackrf import *
 from scipy import signal
 from time import sleep
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 center_mag = []
+rollingData = []
+fullBandAvg = []
+finalBandAvgpxx = []
 values_to_avg = 1
 average_mag = 0
 
+
 with HackRF() as hrf:
     
+    # Desired hackRF settings
     hrf.sample_rate = 20e6
     hrf.center_freq = 1420e6
-    hrf.lna_gain = 8
-    hrf.vga_gain = 22
+    hrf.lna_gain = 40
+    hrf.vga_gain = 8
     num_samples = 2e4
 
-    # loop to gather multiple magnitude values at center frequency and average them
-    for number in range(values_to_avg): 
+    # loop and add power arrays to an array of arrays
+
+    for count in range(values_to_avg):    
         samples = hrf.read_samples(num_samples)
-        
-        f, pxx = signal.welch(samples, hrf.sample_rate/1e6, 'blackman',1024)
+        f, pxx = signal.welch(samples, hrf.sample_rate/1e6, 'blackman', nperseg=20e3,nfft=40e3)
 
-        center_sample_index = len(pxx)/2 - 1 
-
-        center_value = pxx[int(center_sample_index)]
+        rollingData.append(pxx)
     
-        center_mag.append(10*np.log10(center_value))
+    # create an fft corrected frequency array
+    sortedFreq = np.fft.fftshift(f)+1420
+    
+    # Average the power values point by point. (average per index)
+    pointAvgpxx = np.average(np.array(rollingData), axis=0)      
 
-        average_mag = sum(center_mag) / len(center_mag)
+    # acquire the indicies for desired frequency range MHz
+    firstFreqIndex = np.where(sortedFreq == 1419)
+    lastFreqIndex = np.where(sortedFreq == 1421)
 
+    # create a new array with desired averaged values
+    for index,power in enumerate(pointAvgpxx):
+        if index >= firstFreqIndex[0]: 
+            if index > lastFreqIndex[0]:
+                break
+            else: 
+                finalBandAvgpxx.append(power)
+    
+    # Collecting and printing the average across the desired band
+    average_mag = np.average(finalBandAvgpxx)
     print(average_mag)
 
 

PythonScripts/hackRFtrial_spectrumanalyzer_w_avg.py
@@ -33,7 +33,7 @@ def main():
 
             rollingData.append(pxx)
 
-        avg_pxx = np.average(np.array(rollingData), axis=0)      
+        avg_pxx = np.average(np.array(rollingData), axis=0)  
         y_center = hrf.center_freq/1e6
 
         plt.ion()